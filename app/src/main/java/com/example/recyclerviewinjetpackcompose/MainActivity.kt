852a0aa20b0d0cd96e206850f30279f2801b261d
Hp <lubnamariyam@vajro.com>
2021-08-28 21:50:21 -0700

    final commit

--- app/src/main/java/com/example/recyclerviewinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewinjetpackcompose/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.recyclerviewinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
@@ -10,6 +12,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.recyclerviewinjetpackcompose.model.Person
 import com.example.recyclerviewinjetpackcompose.repository.PersonRepository
 import com.example.recyclerviewinjetpackcompose.ui.theme.RecyclerviewInJetpackComposeTheme
@@ -23,7 +26,8 @@ class MainActivity : ComponentActivity() {
                 val personRepository = PersonRepository()
                 val getAllData = personRepository.getAllData()
                 
-                LazyColumn(){
+                LazyColumn(contentPadding = PaddingValues(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)){
                     items(items = getAllData){
                         Person ->  CustomItem(person = Person)
                     }
